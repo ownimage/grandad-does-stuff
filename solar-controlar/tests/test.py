9f56b61eeacbce049bf733f345e3094b65dc890b
Keith Hart <keith@ownimage.co.uk>
2025-06-02 07:57:11 +0100

    Woodpecker CI solar-controlar test branch

--- solar-controlar/tests/test.py
+++ solar-controlar/tests/test.py
@@ -7,5 +7,8 @@ class TestSum(unittest.TestCase):
     def test_list_int2(self):
         self.assertEqual(1, 1)
 
+    def test_list_int3(self):
+        self.assertEqual(1, 1)
+
 if __name__ == '__main__':
     unittest.main()
