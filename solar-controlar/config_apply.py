aa068586ccc28871f5f1c7d09ed40379ac63f408
Keith Hart aka ownimage <keith@ownimage.co.uk>
2025-06-07 14:49:45 +0100

    Solar controlar tests phase 1

--- solar-controlar/config_apply.py
+++ solar-controlar/config_apply.py
@@ -2,6 +2,7 @@
 
 import os
 import datetime
+import logging
 
 import pytz
 from tenacity import retry, stop_after_attempt, wait_fixed
@@ -9,21 +10,27 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 from solarcontrolar.config import Config
 from solarcontrolar.givenergy import GivEnergy
 
-tolerance = 1 # TODO should be in settings
-config = Config()
+# Setup logger for better testability
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-api_key = os.getenv("GIVENERGY_API_KEY")
-inverter_id = os.getenv("GIVENERGY_INVERTER_ID")
-givenergy = GivEnergy(api_key, inverter_id)
+# Configuration injection
+def get_config():
+    return Config()
 
-timezone_str = 'Europe/London' # TODO should be in settings
-local_tz = pytz.timezone(timezone_str)
-now = datetime.datetime.now(local_tz)
-formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-hour = now.hour
+def get_givenergy():
+    api_key = os.getenv("GIVENERGY_API_KEY")
+    inverter_id = os.getenv("GIVENERGY_INVERTER_ID")
+    return GivEnergy(api_key, inverter_id)
+
+# Time handling abstraction
+def get_current_time(timezone_str="Europe/London"):
+    local_tz = pytz.timezone(timezone_str)
+    now = datetime.datetime.now(local_tz)
+    return now, now.strftime("%Y-%m-%d %H:%M:%S")
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(10))
-def apply_config(config_key, comparator, action):
+def apply_config(config, givenergy, config_key, comparator, action, tolerance, now, formatted_date):
     target_percentage = config.get_data()[config_key]
     battery_level = givenergy.battery_level()
     enabled = comparator(battery_level, target_percentage)
@@ -31,26 +38,30 @@ def apply_config(config_key, comparator, action):
     if abs(battery_level - target_percentage) > tolerance:
         result = action(enabled)
         if result:
-            print(f'{formatted_date} battery_leval={battery_level} target_percentage={target_percentage} CHANGE {action.__name__}({enabled})')
+            msg = f"{formatted_date} battery_level={battery_level} target_percentage={target_percentage} CHANGE {action.__name__}({enabled})"
         else:
-            print(f'{formatted_date} battery_leval={battery_level} target_percentage={target_percentage} {action.__name__}({enabled})')
+            msg = f"{formatted_date} battery_level={battery_level} target_percentage={target_percentage} {action.__name__}({enabled})"
     else:
-        print(f'{formatted_date} battery_leval={battery_level} is within tolerance={tolerance} of target_percentage={target_percentage} NO CHANGE')
+        msg = f"{formatted_date} battery_level={battery_level} is within tolerance={tolerance} of target_percentage={target_percentage} NO CHANGE"
 
+    logger.info(msg)
+    return msg  # Allows assertion in unit tests
 
+# Main function for better testability
+def main():
+    config = get_config()
+    givenergy = get_givenergy()
+    tolerance = 1  # TODO should be in settings
 
-# Then, in your scheduling logic, you can simply do:
-if 2 <= hour < 5:
-    apply_config(
-        config_key="charge_to_percentage",
-        comparator=lambda level, target: level <= target,
-        action=givenergy.set_timed_charge
-    )
-elif 16 <= hour < 19:
-    apply_config(
-        config_key="discharge_to_percentage",
-        comparator=lambda level, target: level >= target,
-        action=givenergy.set_timed_export
-    )
-else:
-    print(f'{formatted_date} no action')
+    now, formatted_date = get_current_time()
+    hour = now.hour
+
+    if 2 <= hour < 5:
+        apply_config(config, givenergy, "charge_to_percentage", lambda lvl, tgt: lvl <= tgt, givenergy.set_timed_charge, tolerance, now, formatted_date)
+    elif 16 <= hour < 19:
+        apply_config(config, givenergy, "discharge_to_percentage", lambda lvl, tgt: lvl >= tgt, givenergy.set_timed_export, tolerance, now, formatted_date)
+    else:
+        logger.info(f"{formatted_date} no action")
+
+if __name__ == "__main__":
+    main()
--- solar-controlar/solarcontrolar/config.py
+++ solar-controlar/solarcontrolar/config.py
@@ -5,20 +5,20 @@ import copy
 class Config:
 
     def __init__(self, filename="config.json", data=None):
-        self.filename = filename;
+        self.__filename = filename
 
         if data is None:
-            with open("config.json", "r") as file:
+            with open(filename, "r") as file:
                 self.__data = json.load(file)
+        else:
+            self.__data = data
 
     def get_data(self):
         return copy.deepcopy(self.__data)
 
     def with_data(self, data):
-        value = Config(self.filename)
-        value.__data = self.get_data()
-        return value
+        return Config(self.__filename, data)
 
     def save(self):
-        with open(self.filename, "w") as file:
+        with open(self.__filename, "w") as file:
             json.dump(self.__data, file, indent=4)
