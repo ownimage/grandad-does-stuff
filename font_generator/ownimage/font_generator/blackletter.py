4f1b89ad277ef54669c53a419d781524a8ef342b
Keith Hart <keith@ownimage.co.uk>
2026-02-02 17:05:16 +0000

    added fontparameter

--- font_generator/ownimage/font_generator/blackletter.py
+++ font_generator/ownimage/font_generator/blackletter.py
@@ -2,29 +2,28 @@ import math
 from vector2d import Vector2D
 
 from .compound_stroke import CompoundStroke
+from .font_parameters import FontParameters
 from .stroke import Stroke, StrokeType
 from .mark import Mark
 from .glyph import Glyph
 
 
 class Blackletter:
-    def __init__(self, pen_thickness: float, b: float, x: float, a: float):
-        self.pen_thickness = pen_thickness
-        self.b = b
-        self.x = x
-        self.a = a
+    def __init__(self, fp: FontParameters):
+
+        self.fp = fp
 
         # calculated values
-        self.m = pen_thickness / (2 * math.sqrt(2))
-        self.xm = x - self.m
-        self.x3m = x - 3 * self.m
-        self.b3m = b + 3 * self.m
+        self.m = fp.pen_thickness / (2 * math.sqrt(2))
+        self.xm = fp.x_height - self.m
+        self.x3m = fp.x_height - 3 * self.m
+        self.b3m = fp.baseline + 3 * self.m
 
         # strokes
         self.s_a1 = Stroke(Vector2D(0, self.b3m - self.x3m), StrokeType.Block)
         self.s_a2 = Stroke(Vector2D(2 * self.m, -2 * self.m), StrokeType.Block)
 
-        self.s_b1 = Stroke(Vector2D(0, self.b3m - self.a), StrokeType.Block)
+        self.s_b1 = Stroke(Vector2D(0, self.b3m - fp.ascender), StrokeType.Block)
 
         self.s_c1 = Stroke(Vector2D(2 * self.m, 2 * self.m), StrokeType.Block)
 
@@ -41,7 +40,7 @@ class Blackletter:
         self.m_a1 = Mark(Vector2D(0, self.x3m), [self.cs_a1])
         self.m_a2 = Mark(Vector2D(2 * self.m, self.xm), [self.cs_a2])
 
-        self.m_b1 = Mark(Vector2D(0, self.a), [self.cs_b1])
+        self.m_b1 = Mark(Vector2D(0, self.fp.ascender), [self.cs_b1])
         self.m_b2 = Mark(Vector2D(2 * self.m, self.xm), [self.cs_b2])
 
         self.m_c1 = Mark(Vector2D(0, self.x3m), [self.cs_c1])
@@ -61,10 +60,10 @@ class Blackletter:
         svg = ""
         start = posn
         for c in chars:
-            svg += self.glyph_map[c].svg(start, scale, self.pen_thickness)
+            svg += self.glyph_map[c].svg(start, scale, self.fp.pen_thickness)
             start += Vector2D(self.glyph_widths[c], 0)
         return svg
 
     def birdfont_path(self, key, scale: float):
         g = self.glyph_map[key]
-        return g.birdfont_path(scale, self.pen_thickness)
+        return g.birdfont_path(scale, self.fp.pen_thickness)
