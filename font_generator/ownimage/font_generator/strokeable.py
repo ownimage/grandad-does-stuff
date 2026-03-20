05f1d103b9bf4558668c5e616a8c4762b9972b2c
Keith Hart <keith@ownimage.co.uk>
2026-03-20 05:14:42 +0000

    parameter tidy up

--- font_generator/ownimage/font_generator/strokeable.py
+++ font_generator/ownimage/font_generator/strokeable.py
@@ -19,9 +19,9 @@ class Strokeable:
     def geometry(self, fp: FontParameters, start: Vector, scale: float, before: Strokeable, after: Strokeable, geom_set: GeometrySet) -> Vector:
         self._not_implemented("geometry")
 
-    def bounding_box(self, fp: FontParameters, start: Vector = Vector(0, 0), scale: float = 1.0):
+    def bounding_box(self, fp: FontParameters, start: Vector = Vector(0, 0), scale: float = 1.0, before: Strokeable = None, after: Strokeable = None):
         geom_set = GeometrySet()
-        self.geometry(fp, start, scale, None, None, geom_set)
+        self.geometry(fp, start, scale, before, after, geom_set)
         return geom_set.bounding_box()
 
     def svg(self, start: Vector, fp: FontParameters, scale: float) -> str:
