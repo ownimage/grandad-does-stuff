dd06ba1ff5b7e52c29b2b360db84fa8ffb00c1a3
Keith Hart <keith@ownimage.co.uk>
2026-03-29 08:50:45 +0100

    Bezier to implement multi stroke

--- font_generator/ownimage/font_generator/mark.py
+++ font_generator/ownimage/font_generator/mark.py
@@ -113,7 +113,7 @@ class Mark:
                 prev_item = self.strokes[i - 1] if i > 0 else None
                 next_item = self.strokes[i + 1] if i < len(self.strokes) - 1 else None
 
-                if isinstance(curr_item, Stroke | BezierStroke | CircleStroke): # TODO
+                if isinstance(curr_item, Strokeable):
                     offset_pos = current_pos + nib.direction * (-0.5 * fp.pen_width + start_offset + 0.5 * width)
                     curr_item.geometry(fpt, offset_pos, scale, prev_item, next_item, geom_set)
                 elif idx == 0:
